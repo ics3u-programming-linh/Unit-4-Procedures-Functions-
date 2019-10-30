f0d35cf013fa581daaf980dc5524fd31ada22e9c
Linh <46288798+s272936@users.noreply.github.com>
2019-10-30 12:58:08 -0400

    Pass

---
+++ PassByValueLinhHo/PassByValueLinhHo/PassByValueForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace PassByValueLinhHo
+{
+    partial class frmPassByValue
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCelsius = new System.Windows.Forms.Label();
+            this.txtCelsius = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCelsius
+            // 
+            this.lblCelsius.AutoSize = true;
+            this.lblCelsius.Location = new System.Drawing.Point(217, 128);
+            this.lblCelsius.Name = "lblCelsius";
+            this.lblCelsius.Size = new System.Drawing.Size(200, 13);
+            this.lblCelsius.TabIndex = 0;
+            this.lblCelsius.Text = "Enter the temperature in degrees Celsius:";
+            // 
+            // txtCelsius
+            // 
+            this.txtCelsius.Location = new System.Drawing.Point(453, 125);
+            this.txtCelsius.Name = "txtCelsius";
+            this.txtCelsius.Size = new System.Drawing.Size(100, 20);
+            this.txtCelsius.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(308, 177);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(143, 37);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Convert to Fahrenheit";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            // 
+            // frmPassByValue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtCelsius);
+            this.Controls.Add(this.lblCelsius);
+            this.Name = "frmPassByValue";
+            this.Text = "Pass by Value";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCelsius;
+        private System.Windows.Forms.TextBox txtCelsius;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
