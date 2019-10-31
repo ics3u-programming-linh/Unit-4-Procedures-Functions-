69e447a0d27d64ec4df694071f61ba396829e794
Linh <46288798+s272936@users.noreply.github.com>
2019-10-31 13:03:04 -0400

    Update

--- PassByValueLinhHo/PassByValueLinhHo/PassByValueForm.cs
+++ PassByValueLinhHo/PassByValueLinhHo/PassByValueForm.cs
@@ -18,23 +18,37 @@ namespace PassByValueLinhHo
         }
 
         // Function: CalculateFahrenheit
-        // Input: double userCelsius
+        // Input: double celsius, double fahrenheit
         // Output: void 
         // Description: This function takes the user's celsius passed by value
         // and displays a message box with the calculation of the fahrenheit
-
         public void CalculateFahrenheit(double celsius, double fahrenheit)
         {
             // declare local variables
-            double userCelsius;
             double fahrenheit;
 
+            // Calculate the fahrenheit
+            fahrenheit = (double)9 / (double)5 * celsius + 32;
+
             // display a message box with the calculated fahrenheit
-            Message.Show("The fahrenheit degrees is" + fahrenheit);
+            MessageBox.Show(" The degrees in fahrenheit is: " + fahrenheit);
         }
         private void BtnCalculate_Click(object sender, EventArgs e)
         {
-           
+            // declare local variables
+            double celsius;
+            double fahrenheit;
+
+            // get user's input in celsius
+
+            // convert the input length and width from String to int and assign
+            // them to their corresponding variables
+
+            celsius = Convert.ToInt32(this.txtCelsius.Text);
+
+            celsius = fahrenheit; 
+            // Call the function to calculate the celsius passing the celsius and fahrenheit arguements by value
+            this.CalculateFahrenheit(celsius, fahrenheit);
         }
     }
 }
