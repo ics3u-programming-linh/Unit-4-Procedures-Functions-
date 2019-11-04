cf34b1d039b44dfa3781006b6168ccbed6c8ab81
Linh <46288798+s272936@users.noreply.github.com>
2019-11-04 13:09:44 -0500

    O

---
+++ OptionalParametersLinhH/OptionalParametersLinhH/OptionalParameters.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by: First Last
+ * Created on: Day-Month-Year
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #32 - Address Program
+ * This program has 2 procedures with the same name (PrintAddress) but different parameters.
+ * User enters their information such as streetAddress, city, province and postal code. 
+ * The program displays the following information as a message box.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OptionalParametersLinhH
+{
+    static class OptionalParameters
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmOptionalParameters());
+        }
+    }
+}
