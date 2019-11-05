f8c5ee6edbd6c126bb0f31f80c5f82b2393d6e61
Linh <46288798+s272936@users.noreply.github.com>
2019-11-05 12:51:01 -0500

    Update

---
+++ FunctionsLinhH/FunctionsLinhH/FunctionsForm.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace FunctionsLinhH
+{
+    partial class frmFunctions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRadius = new System.Windows.Forms.Label();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.txtRadius = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.lblVolume = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblRadius
+            // 
+            this.lblRadius.AutoSize = true;
+            this.lblRadius.Location = new System.Drawing.Point(88, 77);
+            this.lblRadius.Name = "lblRadius";
+            this.lblRadius.Size = new System.Drawing.Size(71, 13);
+            this.lblRadius.TabIndex = 0;
+            this.lblRadius.Text = "Enter Radius:";
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(88, 126);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(69, 13);
+            this.lblHeight.TabIndex = 1;
+            this.lblHeight.Text = "Enter Height:";
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Location = new System.Drawing.Point(165, 74);
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(100, 20);
+            this.txtRadius.TabIndex = 2;
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(165, 123);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.TabIndex = 3;
+            // 
+            // lblVolume
+            // 
+            this.lblVolume.AutoSize = true;
+            this.lblVolume.Location = new System.Drawing.Point(59, 255);
+            this.lblVolume.Name = "lblVolume";
+            this.lblVolume.Size = new System.Drawing.Size(35, 13);
+            this.lblVolume.TabIndex = 4;
+            this.lblVolume.Text = "label3";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(116, 174);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(118, 55);
+            this.btnCalculate.TabIndex = 5;
+            this.btnCalculate.Text = "Calculate the volume of a cylinder";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // frmFunctions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 308);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblVolume);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtRadius);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.lblRadius);
+            this.Name = "frmFunctions";
+            this.Text = "Functions by Linh Ho";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRadius;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.TextBox txtRadius;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Label lblVolume;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ FunctionsLinhH/FunctionsLinhH/FunctionsForm.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * Created by: Linh Ho
+ * Created on: November 5th, 2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #30 - Functions
+ * This program calculates the volume of a cylinder
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FunctionsLinhH
+{
+    public partial class frmFunctions : Form
+    {
+        public frmFunctions()
+        {
+            InitializeComponent();
+
+            // Hide labels
+            lblVolume.Hide();
+        }
+
+        private double CalculateVolCyl (double height , double radius)
+        {
+            // declare variables
+            double volume;
+
+            // calculate the volume of a cyclinder
+            volume = height * Math.Pow(radius, 2) * 3.14;
+
+            return volume;
+        }
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            // declare variables 
+            double userHeight, userRadius, userVolume;
+
+            // get the value from the user of radius and height
+            userHeight = Convert.ToDouble(this.txtHeight.Text);
+            userRadius = Convert.ToDouble(this.txtRadius.Text);
+
+            // call the function to return the volume
+            userVolume = CalculateVolCyl(userHeight, userRadius);
+
+            // Make the label visible
+            lblVolume.Show();
+
+            // Display the volume
+            this.lblVolume.Text = "The volume of the cyclinder is: " + userVolume;
+
+        }
+    }
+}
