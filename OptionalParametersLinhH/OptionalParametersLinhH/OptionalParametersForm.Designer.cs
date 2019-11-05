6a0d58c6d32ac313004b19a7a2030597a8c638bf
Linh <46288798+s272936@users.noreply.github.com>
2019-11-05 13:10:54 -0500

    Update

--- OptionalParametersLinhH/OptionalParametersLinhH/OptionalParametersForm.Designer.cs
+++ OptionalParametersLinhH/OptionalParametersLinhH/OptionalParametersForm.Designer.cs
@@ -30,120 +30,127 @@
         {
             this.lblApartmentNum = new System.Windows.Forms.Label();
             this.lblStreetAddress = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.lblProvince = new System.Windows.Forms.Label();
+            this.lblPostalCode = new System.Windows.Forms.Label();
+            this.txtAptNum = new System.Windows.Forms.TextBox();
+            this.txtStreetAddress = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtProvince = new System.Windows.Forms.TextBox();
+            this.txtPostalCode = new System.Windows.Forms.TextBox();
+            this.btnEnter = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblApartmentNum
             // 
             this.lblApartmentNum.AutoSize = true;
-            this.lblApartmentNum.Location = new System.Drawing.Point(178, 58);
+            this.lblApartmentNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblApartmentNum.Location = new System.Drawing.Point(31, 34);
             this.lblApartmentNum.Name = "lblApartmentNum";
-            this.lblApartmentNum.Size = new System.Drawing.Size(35, 13);
+            this.lblApartmentNum.Size = new System.Drawing.Size(200, 24);
             this.lblApartmentNum.TabIndex = 0;
-            this.lblApartmentNum.Text = "label1";
+            this.lblApartmentNum.Text = "Apt Number (optional):";
             // 
             // lblStreetAddress
             // 
             this.lblStreetAddress.AutoSize = true;
-            this.lblStreetAddress.Location = new System.Drawing.Point(181, 95);
+            this.lblStreetAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStreetAddress.Location = new System.Drawing.Point(34, 71);
             this.lblStreetAddress.Name = "lblStreetAddress";
-            this.lblStreetAddress.Size = new System.Drawing.Size(35, 13);
+            this.lblStreetAddress.Size = new System.Drawing.Size(138, 24);
             this.lblStreetAddress.TabIndex = 1;
-            this.lblStreetAddress.Text = "label2";
+            this.lblStreetAddress.Text = "Street Address:";
             // 
-            // label3
+            // lblCity
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(181, 134);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.lblCity.AutoSize = true;
+            this.lblCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCity.Location = new System.Drawing.Point(34, 110);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(45, 24);
+            this.lblCity.TabIndex = 2;
+            this.lblCity.Text = "City:";
             // 
-            // label4
+            // lblProvince
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(184, 173);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "label4";
+            this.lblProvince.AutoSize = true;
+            this.lblProvince.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProvince.Location = new System.Drawing.Point(37, 149);
+            this.lblProvince.Name = "lblProvince";
+            this.lblProvince.Size = new System.Drawing.Size(89, 24);
+            this.lblProvince.TabIndex = 3;
+            this.lblProvince.Text = "Province:";
             // 
-            // label5
+            // lblPostalCode
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(184, 206);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "label5";
+            this.lblPostalCode.AutoSize = true;
+            this.lblPostalCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPostalCode.Location = new System.Drawing.Point(37, 182);
+            this.lblPostalCode.Name = "lblPostalCode";
+            this.lblPostalCode.Size = new System.Drawing.Size(116, 24);
+            this.lblPostalCode.TabIndex = 4;
+            this.lblPostalCode.Text = "Postal Code:";
             // 
-            // textBox1
+            // txtAptNum
             // 
-            this.textBox1.Location = new System.Drawing.Point(250, 58);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 5;
+            this.txtAptNum.Location = new System.Drawing.Point(237, 38);
+            this.txtAptNum.Name = "txtAptNum";
+            this.txtAptNum.Size = new System.Drawing.Size(100, 20);
+            this.txtAptNum.TabIndex = 5;
             // 
-            // textBox2
+            // txtStreetAddress
             // 
-            this.textBox2.Location = new System.Drawing.Point(250, 95);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 6;
+            this.txtStreetAddress.Location = new System.Drawing.Point(237, 76);
+            this.txtStreetAddress.Name = "txtStreetAddress";
+            this.txtStreetAddress.Size = new System.Drawing.Size(100, 20);
+            this.txtStreetAddress.TabIndex = 6;
             // 
-            // textBox3
+            // txtCity
             // 
-            this.textBox3.Location = new System.Drawing.Point(250, 134);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 7;
+            this.txtCity.Location = new System.Drawing.Point(237, 112);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(100, 20);
+            this.txtCity.TabIndex = 7;
             // 
-            // textBox4
+            // txtProvince
             // 
-            this.textBox4.Location = new System.Drawing.Point(250, 173);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 8;
+            this.txtProvince.Location = new System.Drawing.Point(237, 154);
+            this.txtProvince.Name = "txtProvince";
+            this.txtProvince.Size = new System.Drawing.Size(100, 20);
+            this.txtProvince.TabIndex = 8;
             // 
-            // textBox5
+            // txtPostalCode
             // 
-            this.textBox5.Location = new System.Drawing.Point(250, 206);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 20);
-            this.textBox5.TabIndex = 9;
+            this.txtPostalCode.Location = new System.Drawing.Point(237, 184);
+            this.txtPostalCode.Name = "txtPostalCode";
+            this.txtPostalCode.Size = new System.Drawing.Size(100, 20);
+            this.txtPostalCode.TabIndex = 9;
             // 
-            // button1
+            // btnEnter
             // 
-            this.button1.Location = new System.Drawing.Point(181, 297);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnter.Location = new System.Drawing.Point(106, 244);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(149, 44);
+            this.btnEnter.TabIndex = 10;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.BtnEnter_Click);
             // 
             // frmOptionalParameters
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
+            this.ClientSize = new System.Drawing.Size(385, 434);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.txtPostalCode);
+            this.Controls.Add(this.txtProvince);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtStreetAddress);
+            this.Controls.Add(this.txtAptNum);
+            this.Controls.Add(this.lblPostalCode);
+            this.Controls.Add(this.lblProvince);
+            this.Controls.Add(this.lblCity);
             this.Controls.Add(this.lblStreetAddress);
             this.Controls.Add(this.lblApartmentNum);
             this.Name = "frmOptionalParameters";
@@ -157,15 +164,15 @@
 
         private System.Windows.Forms.Label lblApartmentNum;
         private System.Windows.Forms.Label lblStreetAddress;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.Label lblProvince;
+        private System.Windows.Forms.Label lblPostalCode;
+        private System.Windows.Forms.TextBox txtAptNum;
+        private System.Windows.Forms.TextBox txtStreetAddress;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtProvince;
+        private System.Windows.Forms.TextBox txtPostalCode;
+        private System.Windows.Forms.Button btnEnter;
     }
 }
 
--- OptionalParametersLinhH/OptionalParametersLinhH/OptionalParametersForm.cs
+++ OptionalParametersLinhH/OptionalParametersLinhH/OptionalParametersForm.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Created by: Linh Ho
+ * Created on: November 5th, 2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #29 - Optional Parameters
+ * This program... has 2 procedures with the same name (PrintAddress) but different parameters. 
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +22,42 @@ namespace OptionalParametersLinhH
         public frmOptionalParameters()
         {
             InitializeComponent();
+        }
+
+        // Procedure: PrintAddress
+        // Input: String aptNumber, String streetAddress, String city, String province, String postalCode
+        // Output: void
+        // Description: This procedure displays a message box with the given five parameters: 
+        // aptNumber, streetAddress, city, province and postalCode
+        public void PrintAddress(String aptNumber, String streetAddress, String city, String province, String postalCode)
+        {
+            // Display the address
+            MessageBox.Show("Your address is:" + aptNumber + " " + streetAddress + " " + city + " " + province + " " + postalCode);
+        }
+
+        // Procedure: PrintAddress
+        // Input: String streetAddress, String city, String province, String postalCode
+        // Output: void
+        // Description: This procedure displays a message box with the given four parameters: 
+        // streetAddress, city, province and postalCode
+        public void PrintAddress(String aptNumber, String streetAddress, String city, String province, String postalCode)
+        {
+            // Display the address
+            MessageBox.Show("Your address is:" + " " + streetAddress + " " + city + " " + province + " " + postalCode);
+        }
+
+        private void BtnEnter_Click(object sender, EventArgs e)
+        {
+            // declare variables
+            String aptNumber, streetAddress, city, province, postalCode;
+
+            // get the info from text boxes
+            aptNumber = this.txtAptNum.Text;
+            streetAddress = this.txtAptNum.Text;
+            city = this.txtCity.Text;
+            province = this.txtProvince.Text;
+            postalCode = this.txtPostalCode.Text;
+
         }
     }
 }
