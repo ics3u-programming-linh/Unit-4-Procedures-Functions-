c75ed38254cddd437c4c625849a5e547b42ca4ff
Linh <46288798+s272936@users.noreply.github.com>
2019-11-06 08:52:28 -0500

    Update optional

--- OptionalParametersLinhH/OptionalParametersLinhH/OptionalParametersForm.cs
+++ OptionalParametersLinhH/OptionalParametersLinhH/OptionalParametersForm.cs
@@ -40,7 +40,7 @@ namespace OptionalParametersLinhH
         // Output: void
         // Description: This procedure displays a message box with the given four parameters: 
         // streetAddress, city, province and postalCode
-        public void PrintAddress(String aptNumber, String streetAddress, String city, String province, String postalCode)
+        public void PrintAddress(String streetAddress, String city, String province, String postalCode)
         {
             // Display the address
             MessageBox.Show("Your address is:" + " " + streetAddress + " " + city + " " + province + " " + postalCode);
@@ -49,14 +49,43 @@ namespace OptionalParametersLinhH
         private void BtnEnter_Click(object sender, EventArgs e)
         {
             // declare variables
-            String aptNumber, streetAddress, city, province, postalCode;
+            String userAptNumber, userStreetAddress, userCity, userProvince, userPostalCode;
 
             // get the info from text boxes
-            aptNumber = this.txtAptNum.Text;
-            streetAddress = this.txtAptNum.Text;
-            city = this.txtCity.Text;
-            province = this.txtProvince.Text;
-            postalCode = this.txtPostalCode.Text;
+            userAptNumber = this.txtAptNum.Text;
+            userStreetAddress = this.txtStreetAddress.Text;
+            userCity = this.txtCity.Text;
+            userProvince = this.txtProvince.Text;
+            userPostalCode = this.txtPostalCode.Text;
+
+            // make sure if user enters streetAddress
+            if (userStreetAddress == "")
+            {
+                // show a message box to enter street address
+                MessageBox.Show("Please enter your street address.");
+            }
+            // make sure user enters a city
+            else if (userCity == "")
+            {
+                // show a message to enter province
+                MessageBox.Show("Please enter your province");
+            }
+            // make sure user enters a postal code
+            else if (userPostalCode == "")
+            {
+                // show a message to enter postal code
+                MessageBox.Show("Please enter your postal code");
+            }
+            // if the user does not enter aptNumber, call the procedure that accepts only 4 string parameters
+            else if (userAptNumber == "")
+            {
+                PrintAddress(userStreetAddress, userCity, userProvince, userPostalCode);
+            }
+            // if the user enters all of the information, call the procedure that accepts 5 string parameters
+            else
+            {
+                PrintAddress(userAptNumber, userStreetAddress, userCity, userProvince, userPostalCode);
+            }
 
         }
     }
